c0ca550142f3510d686a148e39e83528cefd8248
Douglas Tarr <douglas.tarr@gmail.com>
2013-05-22 12:06:13 -0700

    bump version + rename solution

--- SeedEquity.Services.Email.Ninject/Properties/AssemblyInfo.cs
+++ SeedEquity.Services.Email.Ninject/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SeedEquity.Services.Email.Ninject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET Email Binding with Ninject / Debug Mode")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SeedEquity")]
 [assembly: AssemblyProduct("SeedEquity.Services.Email.Ninject")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
